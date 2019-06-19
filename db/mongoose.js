997fe39d40b6e2d901fc7268d6b197d1ff606eef
Eduard Valiyllin <eduardworrk@gmail.com>
2019-06-19 17:15:19 +0300

    test

--- db/mongoose.js
+++ db/mongoose.js
@@ -1,6 +1,6 @@
 var mongoose = require('mongoose');
 var config = require('../config.js');
-var db = mongoose.connect(config.mongo.url, {
+var db = mongoose.connect("mongodb://eduardworrk@gmail.com:shapa060708Рx1@ds119738.mlab.com:19738/heroku_1sl1m4q3", {
     useMongoClient: true,
     /* other options */
 });
@@ -8,7 +8,7 @@ var db = mongoose.connect(config.mongo.url, {
 db.on('error', console.error.bind(console, 'Mongo connection error:'));
 
 db.once('open', function callback () {
-    console.log('Mongo ' + config.mongo.url + ' connected!');
+    //console.log('Mongo ' + config.mongo.url + ' connected!');
 });
 mongoose.Promise = global.Promise;
 module.exports = mongoose;
